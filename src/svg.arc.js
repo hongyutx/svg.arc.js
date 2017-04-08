9771e0d0f5c1e3ed55dda9e416a83fc82945584f
Hong Yu <hongyutx@gmail.com>
2017-04-08 16:16:56 -0500

    arc: replace let with var

--- src/svg.arc.js
+++ src/svg.arc.js
@@ -53,8 +53,8 @@ SVG.extend(SVG.Line, {
 
 SVG.extend(SVG.Point, {
   distance: function(pt) {
-      let dx = this.x - pt.x;
-      let dy = this.y - pt.y;
+      var dx = this.x - pt.x;
+      var dy = this.y - pt.y;
       return Math.sqrt(dx*dx + dy*dy);
   }
   
@@ -62,10 +62,10 @@ SVG.extend(SVG.Point, {
         return Math.abs((pt2.y-pt1.y)*this.x - (pt2.x-pt1.x)*this.y + pt2.x*pt1.y - pt2.y*pt1.x)/Math.sqrt((pt2.y-pt1.y)*(pt2.y-pt1.y) + (pt2.x-pt1.x)*(pt2.x-pt1.x));
     }
   , withinLineRange: function(pt1, pt2) {
-        let minX = Math.min(pt1.x, pt2.x);
-        let minY = Math.min(pt1.y, pt2.y);
-        let maxX = Math.max(pt1.x, pt2.x);
-        let maxY = Math.max(pt1.y, pt2.y);
+        var minX = Math.min(pt1.x, pt2.x);
+        var minY = Math.min(pt1.y, pt2.y);
+        var maxX = Math.max(pt1.x, pt2.x);
+        var maxY = Math.max(pt1.y, pt2.y);
 
         return this.x >= minX - Trig.CLOSE_ENOUGH_DISTANCE &&
                 this.x <= maxX + Trig.CLOSE_ENOUGH_DISTANCE &&
@@ -115,7 +115,7 @@ SVG.Arc = SVG.invent({
   // Add class methods
 , extend: {
   plotRadius: function(x1, y1, r, largeArcFlag, sweepFlag, x2, y2) {
-        let p = 'M {0},{1} A {2},{2} 0 {3},{4} {5},{6}'.format(x1.toFixed(2), 
+        var p = 'M {0},{1} A {2},{2} 0 {3},{4} {5},{6}'.format(x1.toFixed(2), 
                                         y1.toFixed(2),
                                         r.toFixed(2),
                                         largeArcFlag.toFixed(0),
@@ -135,9 +135,9 @@ SVG.Arc = SVG.invent({
     
   , plot: function(x1, y1, h, sweepFlag, x2, y2) {
       //cord length
-      let lc = Math.sqrt((x1 - x2)*(x1 - x2) + (y1 - y2)*(y1 - y2));
+      var lc = Math.sqrt((x1 - x2)*(x1 - x2) + (y1 - y2)*(y1 - y2));
       //cord lineAngle
-      let ac = this.lineAngle(x1, y1, x2, y2);
+      var ac = this.lineAngle(x1, y1, x2, y2);
       //now calculate radius of the arc
       this.r = (lc/2.0 * lc/2.0 + h*h) / (2.0*h);
       //arc lineAngle
@@ -146,7 +146,7 @@ SVG.Arc = SVG.invent({
       else
         this.ang = Math.PI + 2.0 * Math.acos(lc/(2.0*this.r));
 
-      let phi = Math.PI*0.5 + ac - this.ang*0.5;
+      var phi = Math.PI*0.5 + ac - this.ang*0.5;
       this.center.x = x1 + this.r * Math.cos(phi);
       this.center.y = y1 + this.r * Math.sin(phi);
       
@@ -156,7 +156,7 @@ SVG.Arc = SVG.invent({
       this.y11 = y1;
       this.x12 = x2;
       this.y12 = y2;
-      let p = 'M {0},{1} A {2},{2} 0 {3},{4} {5},{6}'.format(x1.toFixed(2), 
+      var p = 'M {0},{1} A {2},{2} 0 {3},{4} {5},{6}'.format(x1.toFixed(2), 
                                         y1.toFixed(2),
                                         this.r.toFixed(2),
                                         this.largeArcFlag.toFixed(0),
@@ -166,7 +166,7 @@ SVG.Arc = SVG.invent({
       return this.attr('d', (this._array = new SVG.PathArray(p)));
     }
 , lineAngle(x1, y1, x2, y2) {
-      let t = Math.atan2(y2 - y1, x2 - x1);
+      var t = Math.atan2(y2 - y1, x2 - x1);
       while (t < 0)
         t += 2*Math.PI;
        
@@ -176,7 +176,7 @@ SVG.Arc = SVG.invent({
   //x = cx + rx*cos(theta)
   //y = cy + ry*sin(theta)  
   if (this.bbox().contains(pt)) {
-    let theta = this.lineAngle(this.cx(), this.cy(), pt.x, pt.y);
+    var theta = this.lineAngle(this.cx(), this.cy(), pt.x, pt.y);
 
     x = this.cx() + this.r*Math.cos(theta);
     y = this.cy() + this.r*Math.sin(theta);
@@ -185,13 +185,13 @@ SVG.Arc = SVG.invent({
   return false;
 }
 , mid: function() {
-      let cordMidX = (this.x11 + this.x12)/2.0;
-      let cordMidY = (this.y11 + this.y12)/2.0;
-      let h = this.r - this.r * Math.cos(this.ang/2.0);
-      let sign = this.sweepFlag === 0 ? 1 : -1;
-      let theta = this.lineAngle(this.x11, this.y11, this.x12, this.y12);
-      let cx = cordMidX - Math.sin(theta) * h * sign;
-      let cy = cordMidY + Math.cos(theta) * h * sign; 
+      var cordMidX = (this.x11 + this.x12)/2.0;
+      var cordMidY = (this.y11 + this.y12)/2.0;
+      var h = this.r - this.r * Math.cos(this.ang/2.0);
+      var sign = this.sweepFlag === 0 ? 1 : -1;
+      var theta = this.lineAngle(this.x11, this.y11, this.x12, this.y12);
+      var cx = cordMidX - Math.sin(theta) * h * sign;
+      var cy = cordMidY + Math.cos(theta) * h * sign; 
       return new SVG.Point(cx, cy);
     }
 , length: function() {
